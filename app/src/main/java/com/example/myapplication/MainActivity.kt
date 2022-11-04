b6da7bcd023e42655a2a458f6f007e94421460ad
jp2137 <sbaiyassine2004@gmail.com>
2022-11-04 15:11:29 +0100

    dodanie image button i on click

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageButton>(R.id.imageButton).setOnClickListener {
+            Toast.makeText(this@MainActivity,"To jest bagieta",Toast.LENGTH_SHORT).show()
+        }
+        findViewById<ImageButton>(R.id.imageButton2).setOnClickListener {
+            Toast.makeText(this@MainActivity,"To jest helikopter",Toast.LENGTH_SHORT).show()
+        }
+        findViewById<ImageButton>(R.id.imageButton3).setOnClickListener {
+            Toast.makeText(this@MainActivity,"To jest Jan Paswel II",Toast.LENGTH_SHORT).show()
+        }
+        findViewById<ImageButton>(R.id.imageButton4).setOnClickListener {
+            Toast.makeText(this@MainActivity,"To jest pudliszki",Toast.LENGTH_SHORT).show()
+        }
+        findViewById<ImageButton>(R.id.imageButton5).setOnClickListener {
+            Toast.makeText(this@MainActivity,"To jest żul",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
